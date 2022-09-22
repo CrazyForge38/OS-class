b0fe241a3db6dfe141b596d57a614d58403297bc
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2022-09-22 17:46:47 -0500

    Update client.c

--- client.c
+++ client.c
@@ -10,112 +10,93 @@
 #include <fcntl.h> //allows you to open named pipes
 
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) //lets st
 {
-  static int memory = 0;
-
   if (mkfifo("mainFIFO", 0777) == -1) { //returns 1 if the fifo files returns an error when trying to make it
     if (errno != EEXIST) { //if it does not already exist
       printf("Could not create fifo file\n"); 
       return 1;
     }
   }
-  int fd = open ("mainFIFO", O_RDONLY); //creates a fifo serverfifo in read only but not calling it
-  if (fd == -1) {return 2;} //this lets me know if the fifo was not created properly
-
-  int pid,scn,param_c,value1,value2;
-  char str[30];
-
-
+  
+  int fd = open("mainFIFO", O_WRONLY);
+  if (fd == -1) {printf("0"); return 2; }
+  int usrinput1,usrinput2 = 0;
+  int pid,scn,param_c,value1,value2,results;
+  scn =1;
+  param_c = 1;
+  pid = getpid();
+  
+  if (write(fd, &pid, sizeof(int)) == -1) { return 3; }
+  printf("pid: %d\n", pid);
+  if (write(fd, &scn, sizeof(int)) == -1) { return 3; }
+  printf("scn: %d\n", scn);
+  if (write(fd, &param_c, sizeof(int)) == -1) { return 3; }
+  printf("param_c: %d\n", param_c);
+  printf("-----------------------\n");
+  
   while (1) {
-    open ("mainFIFO", O_RDONLY);
-    if(read(fd, &pid, sizeof(int)) == -1) { return 3; } //
-    printf("read pid: %d\n", pid);
-    if(read(fd, &scn, sizeof(int)) == -1) { return 3; } //
-    printf("read scn: %d\n", scn);
-    if(read(fd, &param_c, sizeof(int)) == -1) { return 3; } //
-    printf("read param_c: %d\n", param_c);
-/////////////////////////////////////////////////////////////////    
-    if(param_c == 1) {//for 1 or 6
-      if (scn == 6) { // for 6
-       if(read(fd, &value1, sizeof(int)) == -1) { return 3; }
-        printf("read value1: %d\n", value1); // grabed storing value
-      } else { // for 1
-        // do nothing
-        }
-      }
-////////////////////////////////////////////////
-    if (param_c == 0) {
-      //Do nothing
-    }
-    //////////////////////////////////////
-    if(param_c == 2 || param_c == 3 || param_c == 4 || param_c == 5) {
-      if(read(fd, &value1, sizeof(int)) == -1) { printf("read val1\n"); return 3; }
-      printf("read value1: %d\n", value1);        
-      if(read(fd, &value2, sizeof(int)) == -1) { printf("read val2\n"); return 3; }
-      printf("read value2: %d\n", value2);
-    }
-    //////////////////////////////////////
-    close(fd);
-    printf("Client pid: %d\n", pid);
-    printf("System Call Requested: %d with %d parameter\n", scn, param_c);
-    int sum,diff,mulp,div = 0;
-    open ("mainFIFO", O_WRONLY); 
-    switch(scn) {
-      case 1 :
-         printf("Client");
-         printf("%d", pid);
-         printf("FIFO\n");
-         break;
-      case 2 :
-         printf("param1: %d and param2: %d\n", value1, value2);
-         sum = value1 + value2;
-         if(write(fd, &sum, sizeof(int)) == -1) { return 4; }
-         printf("Wrote %d\n", sum);
-         break;
-      case 3 :
-         printf("param1: %d and param2: %d\n", value1, value2);
-         diff = value1 - value2;
-         if(write(fd, &diff, sizeof(int)) == -1) { return 4; }
-         printf("Wrote %d\n", diff);
-         break;
-      case 4 :
-         printf("param1: %d and param2: %d\n", value1, value2);
-         mulp = value1 * value2;
-         if(write(fd, &mulp, sizeof(int)) == -1) { return 4; }
-         printf("Wrote %d\n", mulp);
-         break;
-      case 5 :
-          printf("param1: %d and param2: %d\n", value1, value2);
-         div = value1 / value2;
-         if(write(fd, &div, sizeof(int)) == -1) { return 4; }
-         printf("Wrote %d\n", div);
-         break;
-      case 6 :
-         memory = value1;
-         printf("value1 = %d\n", value1);
-         printf("storing: %d\n", memory);
-         if(write(fd, &memory, sizeof(int)) == -1) { return 4; }
-         printf("saved to memory\n");
-         break;
-      case 7 :
-         if(write(fd, &memory, sizeof(int)) == -1) { return 4; }
-         printf("Recalled: %d", memory);
-         break;
-      case 0 :
-         printf("closing the client\n"); //close the client specific fifo and open the next
-         break;
-      case -1 :
-         printf("CLosing the server and client");
-         return 0; //closing the server
-         break;
-      default :
-         printf("Invalid scn\n" );
-   } 
-   printf("--------------------------");
-   printf("\n");
-   close(fd);
+  if (write(fd, &pid, sizeof(int)) == -1) { return 3; } //write pid
+  //Now we are asking the user to select what they want
+  printf("pid: %d\n", pid);
+  printf("enter a number -1 to 7: ");
+  scanf("%d", &scn); 
+  printf("scn input: %d\n", scn);
+  //they all will send the pid and the a SCN
+  if (write(fd, &scn, sizeof(int)) == -1) { return 3; } //write usrinput
+  //sectioning them off by param_c
+  
+  if(scn == 1) {
+    param_c = 1;
+    if (write(fd, &param_c, sizeof(int)) == -1) { return 3; }
   }
-
+  if (scn == 6) { // without 1 bc thats only called once
+    param_c = 1; // 1 param
+    if (write(fd, &param_c, sizeof(int)) == -1) { return 3; }
+    printf("Enter a number: ");
+    scanf("%d", &usrinput2);
+    if (write(fd, &usrinput2, sizeof(int)) == -1) { return 3; } // sending 1 param 
+  }
+  if (scn == 0 || scn == -1 || scn ==7) {
+  }
+  
+  if (scn == 2 || scn == 3 || scn == 4 || scn == 5) {
+    param_c = 2;
+    if (write(fd, &param_c, sizeof(int)) == -1) { return 3; }
+    printf("Enter your first number: ");
+    scanf("%d", &usrinput1);
+    if (write(fd, &usrinput1, sizeof(int)) == -1) {  return 3;}
+    printf("Enter your second number: ");
+    scanf("%d", &usrinput2);
+    if (write(fd, &usrinput2, sizeof(int)) == -1) { return 3;}
+  }
+  close(fd);
+  open ("mainFIFO", O_RDONLY); 
+  
+  if (scn == 2 || scn == 3 || scn == 4 || scn == 5) {
+    if(read(fd, &results, sizeof(int)) == -1) {return 3;  } 
+    printf("Returned: %d\n", results);
+  }
+  if(scn == 6) {
+    if(read(fd, &results, sizeof(int)) == -1) { return 3; } 
+    printf("We saved: %d\n", results);
+  }
+  if(scn == 7) {
+    if(read(fd, &results, sizeof(int)) == -1) { return 3; } 
+    printf("We saved: %d\n", results);
+  }
+  if(scn == 0) {
+    return 0;
+  }
+  if (scn == -1) {
+    return 0;
+  }
+  if(scn == 1) {
+    printf("connection made\n");
+  }
+  printf("------------------------------------\n");
+  close(fd);
+  open("mainFIFO", O_WRONLY);
+}
   return 0;
 }
