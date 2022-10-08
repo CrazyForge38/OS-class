39ad3dad1f6ca847eadc670e18accf17ab7efab5
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2022-10-08 15:51:56 -0500

    Update Client.c
    added debugging ifs

--- Program2/Client.c
+++ Program2/Client.c
@@ -52,75 +52,73 @@ while(1){
 	  int choice;
 	  printf("Select a state for pid %d:\n 1-Send request to server\n 2-Exit client\n 3-Terminate server connection\n => ", pid);
 	  scanf("%d", &choice);
-	  write(writer, &pid, sizeof(int));
+	  if (write(writer, &pid, sizeof(int)) == -1) {return 3;}
 	if (choice == 1){
 		printf("\n Select a SCN: \n  2- add\n  3- subtract\n  4- multiply\n  5- divide\n  6- store\n  7- recall\n  =>");
 		scanf("%d",&scn);
 		printf("\nSending SCN: %d\n", scn);
-		write(writer, &scn, sizeof(int));
+		if (write(writer, &scn, sizeof(int)) == -1) {return 3;}
 	}
 	if (choice == 2){
 		scn = 0;
 		printf("System call 0 sent, exiting now\n");
-		write(writer, &scn, sizeof(int));
+		if (write(writer, &scn, sizeof(int)) == -1) {return 3;}
 		
 		}
 	if (choice == 3){
 		scn= -1;
 		printf("system call -1 sent, terminating now\n");
-		write(writer, &scn, sizeof(int));	
+		if (write(writer, &scn, sizeof(int)) == -1) {return 3;}	
 	}
 }
 
 	if (scn == 6){
 		param_c = 1;
-		write(writer, &param_c, sizeof(int));
+		if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
 		printf("Val1: ");
 		scanf("%d", &val1);
-		write(writer, &val1, sizeof(int));
+		if (write(writer, &val1, sizeof(int)) == -1) {return 4;}
 		
 	}
 	if (scn == 2 || scn == 3 || scn == 4 || scn == 5) {
 		param_c = 2;
-		write(writer, &param_c, sizeof(int));
+		if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
 		
 		printf("Val1: ");
 		scanf("%d", &val1);
-		write(writer, &val1, sizeof(int));
+		if (write(writer, &val1, sizeof(int)) == -1) {return 4;}
 		
 		printf("Val2: ");
 		scanf("%d", &val2);
-		write(writer, &val2, sizeof(int));
+		if (write(writer, &val2, sizeof(int)) == -1) {return 4;}
 		
 
 	}
 	if(scn == 7 || scn == 1){
 		param_c = 0;
-		write(writer, &param_c, sizeof(int));
+		if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
 		}
 
 	if(scn == 0 || scn ==-1){
 		param_c = 0;
-		write(writer, &param_c, sizeof(int));
+		if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
 	}
-	
-
 	
 	close(writer);
 	int reader = open(strPid, O_RDONLY);
 	if (reader == -1) { return 2; }
 	
 	if(scn == 7){
-		read(reader, &results, sizeof(int));
+		if (read(reader, &results, sizeof(int)) == -1) {return 5;}
 		printf("Recalled: %d\n", results);
 	}
 	
 	if (scn == 2 || scn == 3 || scn == 4 || scn == 5) {
-	read(reader, &results, sizeof(int));
+	  if (read(reader, &results, sizeof(int)) == -1) {return 5;}
 		printf("returned result: %d\n",results);
 	}
 	if(scn == 6){
-	read(reader, &results, sizeof(int));
+	  if (read(reader, &results, sizeof(int)) == -1) {return 5;}
 		printf("Stored: %d\n", results);
 	}
 	if(scn == 0 || scn == -1){
@@ -134,7 +132,7 @@ while(1){
 	}
 	
 	if(scn == 1){
-	read(reader, &pid, sizeof(int));
+	if (read(reader, &pid, sizeof(int)) == -1) {return 6;}
 	printf("New ID: %d\n", pid);
 	}
 		scn = 0;
