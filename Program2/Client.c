1434bfe4c16f8adb1fa5c261bfdb0bdb9be85bae
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2022-10-08 23:30:03 -0500

    Update Client.c

--- Program2/Client.c
+++ Program2/Client.c
@@ -13,13 +13,13 @@
 int main(int argc, char* argv[])
 {
 	char strPid[5];
-	int pid, scn, val1, val2, results, param_c, cont;  
+	int pid, scn, val1, val2, results, param_c, cont,scn_State2;  
 	pid = getpid(); 
 
 	sprintf(strPid, "%d", pid);
 
 
-	if (mkfifo(strPid, 0777) == -1) { //error checking
+	if (mkfifo(strPid, 0777) == -1) { //if it does not already exist
 		  if (errno != EEXIST) { //checks if it exists
 		printf("Could not create fifo file\n"); 
 		    return 1;
@@ -39,47 +39,47 @@ int main(int argc, char* argv[])
 	scanf("%d", &scn);
 
 	printf("Sending PID: %d to server\n", pid);
-
+///////////////////////////////////////////////////////////////////////
 	while(1){
-		open("Server", O_WRONLY);
+		open("Server", O_WRONLY); //contacts the server
 		if(scn == 1){
 			printf("[+] initiating connection with server...\n");
 			if (write(writer, &pid, sizeof(int)) == -1) { return 3; } 
 			printf("SCN: %d\n", scn);
 			if (write(writer, &scn, sizeof(int)) == -1) {return 3;}
 		}
-		else{
-			int choice;
+		else{ //select the state the client will move to
 			printf("Select a state for pid %d:\n 1-Send request to server\n 2-Exit client\n 3-Terminate server connection\n => ", pid);
-			scanf("%d", &choice);
+			scanf("%d", &scn);
 			if (write(writer, &pid, sizeof(int)) == -1) {return 3;}
-		if (choice == 1){
-			printf("\n Select a SCN: \n  2- add\n  3- subtract\n  4- multiply\n  5- divide\n  6- store\n  7- recall\n  =>");
+		//if i have a if else if statement this should work without the new var. I will try that when i get a chance	
+		
+		if (scn == 1){
+			printf("\n Select a SCN: \n  2- add\n  3- sub\n  4- mul\n  5- div\n  6- store\n  7- recall\n  =>");
 			scanf("%d",&scn);
 			printf("\nSending SCN: %d\n", scn);
 			if (write(writer, &scn, sizeof(int)) == -1) {return 3;}
-		}
-		if (choice == 2){
-			scn = 0;
-			printf("System call 0 sent, exiting now\n");
-			if (write(writer, &scn, sizeof(int)) == -1) {return 3;}
-			
+		}  else {
+		  if (scn == 2){
+		    //printf("does this: %d", scn);
+		  	scn = 0;
+		    printf("exiting client\n");
+		  	if (write(writer, &scn, sizeof(int)) == -1) {return 3;}
+			} else {
+			  if (scn == 3){
+		      //printf("does this: %d", scn);
+		      scn= -1;
+		      printf("terminating connection with Server\n");
+		      if (write(writer, &scn, sizeof(int)) == -1) {return 3;}	
+        }
 			}
-		if (choice == 3){
-			scn= -1;
-			printf("system call -1 sent, terminating now\n");
-			if (write(writer, &scn, sizeof(int)) == -1) {return 3;}	
 		}
-	}
+	}// else if wasnt working but this did..
 
-		if (scn == 6){
-			param_c = 1;
-			if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
-			printf("Val1: ");
-			scanf("%d", &val1);
-			if (write(writer, &val1, sizeof(int)) == -1) {return 4;}
-			
-		}
+
+///////////////////// state 2 end and now write to the server the param_c and values
+
+		
 		if (scn == 2 || scn == 3 || scn == 4 || scn == 5) {
 			param_c = 2;
 			if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
@@ -91,9 +91,16 @@ int main(int argc, char* argv[])
 			printf("Val2: ");
 			scanf("%d", &val2);
 			if (write(writer, &val2, sizeof(int)) == -1) {return 4;}
-			
-
 		}
+		
+		if (scn == 6){
+			param_c = 1;
+			if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
+			printf("Val1: ");
+			scanf("%d", &val1);
+			if (write(writer, &val1, sizeof(int)) == -1) {return 4;}
+		}
+		
 		if(scn == 7 || scn == 1){
 			param_c = 0;
 			if (write(writer, &param_c, sizeof(int)) == -1) {return 4;}
@@ -105,37 +112,46 @@ int main(int argc, char* argv[])
 		}
 		
 		close(writer);
+		
+
+
 		int reader = open(strPid, O_RDONLY);
 		if (reader == -1) { return 2; }
-		
-		if(scn == 7){
-			if (read(reader, &results, sizeof(int)) == -1) {return 5;}
-			printf("Recalled: %d\n", results);
-		}
-		
-		if (scn == 2 || scn == 3 || scn == 4 || scn == 5) {
+		printf("is this a scn: %d", scn);
+		switch(scn){
+		case 1:
+		  if (read(reader, &pid, sizeof(int)) == -1) {return 6;}
+		  printf("New ID: %d\n", pid);
+		  scn = 0;
+		  break;
+		case 2 :
+		case 3 :
+		case 4 :
+		case 5 :
 			if (read(reader, &results, sizeof(int)) == -1) {return 5;}
 			printf("returned result: %d\n",results);
-		}
-		if(scn == 6){
-			if (read(reader, &results, sizeof(int)) == -1) {return 5;}
+			break;
+	  case 7:
+	    if (read(reader, &results, sizeof(int)) == -1) {return 5;}
+			printf("Recalled: %d\n", results);
+			break;
+	  case 6: 
+	    if (read(reader, &results, sizeof(int)) == -1) {return 5;}
 			printf("Stored: %d\n", results);
-		}
-		if(scn == 0 || scn == -1){
-			if(scn == 0)
-				printf("[+] Closing Client\n");
-			else {
+			break;
+	  case 0: 
+	    printf("[+] Closing Client\n");
+	    return 0;
+	    break;
+	  case -1:
 				printf("[+] Killing Server\n");
-			}
 			return 0;
 			break;
-		}
-		
-		if(scn == 1){
-		if (read(reader, &pid, sizeof(int)) == -1) {return 6;}
-		printf("New ID: %d\n", pid);
-		}
-			scn = 0;
+			
+		default:
+			printf("[+] SCN is invlaid\n" );
+			break;
+	}/////////////////////////////////////////////////////////////////////////////////////////////////
 			close(reader);
 		printf("---------------------------------\n");
 	}//end while
